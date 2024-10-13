95430da77d408a4faae8288971938b094697b117
edsonlimacode <edsonlimacode@gmail.com>
2024-10-13 16:10:23 -0300

    adapter de contatos adicionado

---
+++ app/src/main/java/com/edsonlimadev/chatapp/adapters/ContactAdapter.kt
@@ -0,0 +1,55 @@
+package com.edsonlimadev.chatapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.edsonlimadev.chatapp.databinding.ItemContactsBinding
+import com.edsonlimadev.chatapp.model.User
+import com.squareup.picasso.Picasso
+
+class ContactAdapter : Adapter<ContactAdapter.ContactViewHolder>() {
+
+    private var contactsList = emptyList<User>()
+
+    fun setContacts(list: List<User>) {
+        contactsList = list
+        notifyDataSetChanged()
+    }
+
+    inner class ContactViewHolder(
+        private val binding: ItemContactsBinding
+    ) : ViewHolder(binding.root) {
+
+        fun bind(user: User) {
+
+            if (user.avatar.isNotEmpty()) {
+                Picasso.get().load(user.avatar).into(binding.imgContact)
+            }
+
+            binding.textContact.text = user.name
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+
+        val inflate = LayoutInflater.from(parent.context)
+
+        val itemView = ItemContactsBinding.inflate(inflate, parent, false)
+
+        return ContactViewHolder(itemView)
+
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        val contact = contactsList[position]
+        holder.bind(contact)
+    }
+
+
+    override fun getItemCount(): Int {
+        return contactsList.size
+    }
+
+}
--- app/src/main/java/com/edsonlimadev/chatapp/fragments/ContactFragment.kt
+++ app/src/main/java/com/edsonlimadev/chatapp/fragments/ContactFragment.kt
@@ -1,16 +1,37 @@
 package com.edsonlimadev.chatapp.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.edsonlimadev.chatapp.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.edsonlimadev.chatapp.adapters.ContactAdapter
+import com.edsonlimadev.chatapp.constants.Constants
 import com.edsonlimadev.chatapp.databinding.FragmentContactBinding
+import com.edsonlimadev.chatapp.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ContactFragment : Fragment() {
 
     private lateinit var binding: FragmentContactBinding
+
+    private lateinit var contactAdapter: ContactAdapter
+
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    private val db by lazy {
+        FirebaseFirestore.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getContactList()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +44,45 @@ class ContactFragment : Fragment() {
             false
         )
 
+        contactAdapter = ContactAdapter()
+
+        binding.rvContacts.adapter = contactAdapter
+        binding.rvContacts.layoutManager = LinearLayoutManager(context)
+        binding.rvContacts.addItemDecoration(
+            DividerItemDecoration(context, LinearLayoutManager.VERTICAL)
+        )
 
         return binding.root
 
     }
 
+    private fun getContactList() {
+
+        val userLogged = auth.currentUser?.uid
+
+        if (userLogged != null) {
+
+            db.collection(Constants.USERS_COLLECTION)
+                .addSnapshotListener { querySnapshot, _ ->
+
+                    val documents = querySnapshot?.documents
+
+                    val users = mutableListOf<User>()
+
+                    documents?.forEach { documentSnapshot ->
+
+                        val user = documentSnapshot.toObject(User::class.java)
+
+                        if (user != null && user.id != userLogged) {
+                            users.add(user)
+                        }
+                    }
+
+                    if (users.isNotEmpty()) {
+                        contactAdapter.setContacts(users)
+                    }
+                }
+        }
+    }
 
 }
